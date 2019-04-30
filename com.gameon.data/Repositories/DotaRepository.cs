c7bc661b177a21ab2b40b26e7517a9eff01b0f35
jeffvh <jh0791@gmail.com>
2019-04-30 23:44:30 +0100

    connect to mongo db

--- com.gameon.data/Repositories/DotaRepository.cs
+++ com.gameon.data/Repositories/DotaRepository.cs
@@ -1,12 +1,38 @@
-﻿using System;
+﻿using com.gameon.data.Models;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System.Security.Authentication;
+using System.Threading.Tasks;
 
 namespace com.gameon.data.repositories
 {
     public class DotaRepository
     {
-        public DotaRepository()
-        {
+        private readonly IConfiguration _config;
+        private string _connectionString;
+        private string _dbName;
+        private string _collectionName;
+        private IMongoDatabase _db;
+        private IMongoCollection<Dota> _collection;
 
+        public DotaRepository(IConfiguration config)
+        {
+            _config = config;
+
+            var dbSettings = _config.GetSection("DbSettings");
+            _connectionString = dbSettings["ConnectionString"];
+            _dbName = dbSettings["DbName"];
+            _collectionName = dbSettings["DotaCollection"];
+
+            MongoClient client = new MongoClient(_connectionString);
+            if (client != null)
+            {
+                _db = client.GetDatabase(_dbName);
+                _collection = _db.GetCollection<Dota>(_collectionName);
+            }
         }
+
+        // Crud Methods
+        // collection.Find(new BsonDocument()).ToList();
     }
 }
