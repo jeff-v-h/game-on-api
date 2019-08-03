cb6b19a4a2fbeba26b14b865ab0bc0344fd430c1
jeffvh <jeffvh@outlook.com>
2019-08-03 23:22:52 +0100

    do not add turnaments into list of events

--- com.gameon.domain/Managers/GeneralManager.cs
+++ com.gameon.domain/Managers/GeneralManager.cs
@@ -556,13 +556,14 @@ namespace com.gameon.domain.Managers
                 {
                     var startDate = tournament.BeginAt.Value.Date;
 
-                    if (startDate == weekStartDate.Date) events.Today.Add(new EventVM(tournament));
-                    else if (startDate == tomorrow.Date) events.Tomorrow.Add(new EventVM(tournament));
-                    else if (startDate == day3.Date) events.Day3.Add(new EventVM(tournament));
-                    else if (startDate == day4.Date) events.Day4.Add(new EventVM(tournament));
-                    else if (startDate == day5.Date) events.Day5.Add(new EventVM(tournament));
-                    else if (startDate == day6.Date) events.Day6.Add(new EventVM(tournament));
-                    else if (startDate == day7.Date) events.Day7.Add(new EventVM(tournament));
+                    // Add the tournament if within a week of specified date
+                    //if (startDate == weekStartDate.Date) events.Today.Add(new EventVM(tournament));
+                    //else if (startDate == tomorrow.Date) events.Tomorrow.Add(new EventVM(tournament));
+                    //else if (startDate == day3.Date) events.Day3.Add(new EventVM(tournament));
+                    //else if (startDate == day4.Date) events.Day4.Add(new EventVM(tournament));
+                    //else if (startDate == day5.Date) events.Day5.Add(new EventVM(tournament));
+                    //else if (startDate == day6.Date) events.Day6.Add(new EventVM(tournament));
+                    //else if (startDate == day7.Date) events.Day7.Add(new EventVM(tournament));
 
                     // Get matches for tournament if it any of it's days lie within the dates above.
                     bool isWithinWeek = false;
